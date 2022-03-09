4cdcb68b6728ab68674c6cd6e4c3d4ccac3d6aa7
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2022-03-08 22:55:15 -0800

    More commands and a help menu.

--- src/main.py
+++ src/main.py
@@ -37,6 +37,24 @@ intents.presences = True
 client = discord.Client(intents=intents)
 
 
+async def start_bulone(msg, voice):
+    ctx = Context()
+    if ctx.locked():
+        print(f"Ignore command from {msg.author.display_name}")
+        await msg.channel.send("Already buloning. Please try again later or contact phuang1024.")
+        return
+    ctx.lock()
+    print("Starting Bulone, voice:", voice)
+
+    await ctx.init(client, (VOICE_ID if voice else TEXT_ID), voice)
+
+    await msg.channel.send("Starting Bulone on " + ("voice" if voice else "text"))
+    await asyncio.sleep(4)
+    await conv.start(ctx)
+    await ctx.close()
+    ctx.unlock()
+
+
 @client.event
 async def on_ready():
     text = await client.fetch_channel(TEXT_ID)
@@ -57,31 +75,44 @@ async def on_message(msg: discord.Message):
         return
 
     content = msg.content.lower().strip()
-    pat_text = re.compile(r"bulonebot *\( *text *\)")
-    pat_voice = re.compile(r"bulonebot *\( *voice *\)")
+    pat_cmd = re.compile(r"bulonebot *\(.*?\)")
+    pat_arg = re.compile(r"\(.*?\)")
 
-    if pat_text.findall(content) or pat_voice.findall(content):
-        if msg.author.display_name != "phuang1024" and RESTRICTED:
-            print(f"Ignore command from {msg.author.display_name}")
-            await msg.channel.send("You don't have permissions to use this command. Try `sudo`.")
-            return
+    if pat_cmd.findall(content):
+        find = pat_arg.findall(content)
+        arg = find[0][1:-1].strip() if find else ""
 
-        voice = bool(pat_voice.findall(content))
-        ctx = Context()
-        if ctx.locked():
-            print(f"Ignore command from {msg.author.display_name}")
-            await msg.channel.send("Already buloning. Please try again later or contact phuang1024.")
-            return
-        ctx.lock()
-        print("Starting Bulone, voice:", voice)
+        if arg == "":
+            await msg.channel.send(f"Hello {msg.author.display_name}. Try `bulonebot(help)` for more info.")
 
-        await ctx.init(client, (VOICE_ID if voice else TEXT_ID), voice)
+        elif arg == "help":
+            text = "```\n"
+            text += "bulonebot(help):   Show this help message.\n"
+            text += "bulonebot(about):  About BuloneBot.\n"
+            text += "bulonebot(text):   Start Bulone in a text channel.\n"
+            text += "bulonebot(voice):  Start Bulone in a voice channel.\n"
+            text += "bulonebot(math):   Help about math.\n"
+            text += "```"
+            await msg.channel.send(text)
 
-        await msg.channel.send("Starting Bulone on " + ("voice" if voice else "text"))
-        await asyncio.sleep(4)
-        await conv.start(ctx)
-        await ctx.close()
-        ctx.unlock()
+        elif arg == "about":
+            text = "Mr. Bulone is the best English teacher."
+            await msg.channel.send(text)
+
+        elif arg in ("text", "voice"):
+            if msg.author.display_name != "phuang1024" and RESTRICTED:
+                print(f"Ignore command from {msg.author.display_name}")
+                await msg.channel.send("You don't have permissions to use this command. Try `sudo`.\n"
+                    "This is most likely because the bot is disabled for testing.")
+                return
+
+            await start_bulone(msg, arg == "voice")
+
+        elif arg == "math":
+            await msg.channel.send("Try dividing both sides by 0.")
+
+        else:
+            await msg.channel.send(f"Unknown command. Try `bulonebot(help)` for more info.")
 
 
 with open("token.txt", "r") as fp:
